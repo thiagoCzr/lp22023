74d2d088c4fdcd1073c5058f3387ac53dc86ea2a
Aluno <aluno@ensino.umuarama.local>
2023-11-20 22:23:03 -0300

    aula 20 do 11

--- src/projetovendas/ProjetoVendas.java
+++ src/projetovendas/ProjetoVendas.java
@@ -4,6 +4,9 @@
  */
 package projetovendas;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import projetovendas.model.Cidade;
 import projetovendas.model.Cliente;
 import projetovendas.model.Endereco;
@@ -23,52 +26,101 @@ public class ProjetoVendas {
     public static void main(String[] args) {
 //        TelaCidade cidade = new TelaCidade();
 //        cidade.show();
-        TelaEndereco endereco = new TelaEndereco();
-        endereco.show();
+//        TelaEndereco endereco = new TelaEndereco();
+//        endereco.show();
+
+          Cidade cidadeTrabalho = new Cidade();
+          cidadeTrabalho.setCodibge(123);
+          cidadeTrabalho.setNome("Umuarama");
+          
+          Cidade cidadeCasa = new Cidade();
+          cidadeCasa.setCodibge(123);
+          cidadeCasa.setNome("ivatE");
+          
+          Endereco enderecoCasa = new Endereco();
+          enderecoCasa.setBairro("Centro");
+          enderecoCasa.setCidade(cidadeCasa);
+          enderecoCasa.setLogradouro("Av brasil");
+          
+          Endereco enderecoTrampo = new Endereco();
+          enderecoTrampo.setBairro("Centro");
+          enderecoTrampo.setCidade(cidadeTrabalho);
+          enderecoTrampo.setLogradouro("Av parana");
+          
+          Cliente cliente = new Cliente();
+          cliente.setContador(0);
+          cliente.setEmail("email@mail");
+          cliente.setLocalDeTrabalho("casa");
+          cliente.setNome("Cristiano");
+          cliente.setRenda(150000.00);
+          cliente.setStatus(0);
+          cliente.setTipoPessoa("fisica");
+          
+          List<Endereco> listaEnderecosCliente = new ArrayList();
+          listaEnderecosCliente.add(enderecoCasa);
+          listaEnderecosCliente.add(enderecoTrampo);
+          
+          cliente.setEnderecos(listaEnderecosCliente);
+          
+          
+          Cliente cliente2 = new Cliente();
+          cliente2.setContador(0);
+          cliente2.setEmail("email@mail");
+          cliente2.setLocalDeTrabalho("casa");
+          cliente2.setNome("Ana");
+          cliente2.setRenda(18000.00);
+          cliente2.setStatus(0);
+          cliente2.setTipoPessoa("fisica");
+          
+          List<Endereco> listaEnderecosCliente2 = new ArrayList();
+          listaEnderecosCliente2.add(enderecoCasa);
+          listaEnderecosCliente2.add(enderecoTrampo);
+          
+          cliente2.setEnderecos(listaEnderecosCliente2);
+          
+          List<Cliente> listaDeClientes = new ArrayList();
+          listaDeClientes.add(cliente);
+          listaDeClientes.add(cliente2);
+          
+          
+//          for(Cliente verCliente : listaDeClientes){
+//              System.out.println("cliente nome "+verCliente.getNome());
+//              for(Endereco verEndereco : verCliente.getEnderecos()){
+//                  System.out.println("Endereco do cliente "
+//                          +verCliente.getNome()+", "
+//                                  + ""+verEndereco.getCidade().getNome());
+//              }
+//          }
+
+//NAO FAÇAM ISSO SE TIVEREM JUIZO
+//          int tamLista = listaDeClientes.size();
+//          while (tamLista > 0) {
+//           tamLista--;
+//           Cliente clienteWhile = listaDeClientes.get(tamLista);
+//           System.out.println("cliente nome "+clienteWhile.getNome());
+//           int tamListaEndereco = clienteWhile.getEnderecos().size();
+//              while (tamListaEndereco > 0) {
+//                  tamListaEndereco--;
+//                 Endereco enderecoWhile = clienteWhile.getEnderecos().get(tamListaEndereco);
+//                    System.out.println("Endereco do cliente "
+//                         +clienteWhile.getNome()+", "
+//                                  + ""+enderecoWhile.getCidade().getNome());
+//              }
+//            
+//        }
+          
+          
+         
+         
+          
+          
+          
+          
+          
 
     }
     
-    private static Cliente criarCliente(){
-        Cliente cliente = new Cliente();
-        cliente.setContador(0);
-        cliente.setEmail("email@email.com");
-        cliente.setLocalDeTrabalho("casa");
-        cliente.setEndereco(criarEndereco());
-        cliente.setNome("Cristiano");
-        cliente.setRenda(150000.);
-        cliente.setStatus(1);
-        cliente.setTipoPessoa("loco");
-        System.out.println("criou cliente "+cliente.toString());
-        return cliente;
-    }
-
-     private static Endereco criarEndereco() {
-        Endereco endereco = new Endereco();
-        endereco.setBairro("jaboticabeira");
-        endereco.setLogradouro("rua flor de maio");
-        endereco.setCidade(criarCidade());
-        return endereco;
-    }
-    
-       private static Cidade criarCidade() {
-        Cidade cidade = new Cidade();
-        cidade.setCodibge(1265456);
-        cidade.setNome("Umuarama");
-        return cidade;
-    }
-       
-    private static Fornecedor criarFornecedor(){
-        Fornecedor fornecedor = new Fornecedor();
-        fornecedor.setContador(0);
-        fornecedor.setEmail("email@email.com");
-        fornecedor.setEndereco(criarEndereco());
-        fornecedor.setNome("Cristiano");
-        fornecedor.setStatus(true);
-        fornecedor.setTipoPessoa("loco");
-        System.out.println("criou um fornecedor "+fornecedor.toString());
-        return fornecedor;
-    
-    }   
+  
 
 
 }
